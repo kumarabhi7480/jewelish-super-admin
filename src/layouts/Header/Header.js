b7184feb44f791120b633ad9bd6ca510c732197d
Abhishek kumar <kumarabhi7480882787@gmail.com>
2023-10-19 23:15:10 +0530

    header design done

---
+++ src/layouts/Header/Header.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import "./header.css";
+import { FaBars, FaUser } from "react-icons/fa";
+import { NavLink, Navigate, useNavigate } from "react-router-dom";
+import { BiLogOutCircle } from "react-icons/bi";
+// import { useDispatch, useSelector } from "react-redux";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const [userToggle, setUserToggle] = useState(false);
+  const handleUserToggle = () => setUserToggle(!userToggle);
+//   const { isOpen } = useSelector((state) => state.sidebar);
+//   const dispatch = useDispatch();
+//   const toggle = () =>
+//     dispatch({
+//       type: "setIsOpen",
+//       payload: !isOpen,
+//     });
+
+
+  const handleLogout = () => {
+    // localStorage.removeItem("authAdminToken");
+    // navigate("/login");
+    return <Navigate to="/login" replace={true} />;
+  };
+
+  return (
+    <>
+      <header className="header">
+        <div className="header_wrapper">
+          <div>
+            <div className="bars">
+              <FaBars />
+            </div>
+          </div>
+          <div>
+            <FaUser onClick={handleUserToggle} className="user_icon" />
+
+            {userToggle && (
+              <ul className="user_wrapper">
+                <li className="user_list_style">
+                  <NavLink to="/password/change">
+                    <FaUser className="user_list_icon" /> Change Password
+                  </NavLink>
+                </li>
+                <hr className="hr" />
+                <li className="user_list_style">
+                  <NavLink onClick={handleLogout}>
+                    <BiLogOutCircle className="user_list_icon" /> Log Out
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
