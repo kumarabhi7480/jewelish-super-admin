c7d6b77e0a210206d79afcf7fa8c6923f12e1a06
Abhishek kumar <kumarabhi7480882787@gmail.com>
2023-10-30 00:05:44 +0530

    login reducer

--- src/redux/Actions/Auth.js
+++ src/redux/Actions/Auth.js
@@ -9,7 +9,15 @@ export const login =  ({email,password})=> async(dispatch)=>{
         const {data} = await axios.post("admin/login", {email,password});
         console.log(data);
         const {success,message,token,user} = data;
+        if(success == true){
+            localStorage.setItem("authToken", token);
+            localStorage.setItem("user", JSON.stringify(user));
+            dispatch({type: "loginSuccess"});
+        }else{
+            dispatch({type: "loginFail", payload: message});
+        }
     }catch(error){
+        dispatch({type: "loginFail", payload: error.response.data.message});
         console.log(error.message);
     }
 }
