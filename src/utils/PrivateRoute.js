22ec3d367873f1e9148e065d71423d7559b0c480
Abhishek kumar <kumarabhi7480882787@gmail.com>
2023-11-02 23:52:28 +0530

    update private rote

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -2,13 +2,17 @@ import { useNavigate } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { loadUser } from "../redux/Actions/Auth";
 // import { useUserProfileQuery } from "../services/authApi";
 
 function PrivateRoute() {
   const navigate = useNavigate();
-  const { error: isError } = useSelector(state=>state.auth);
+ const  dispatch = useDispatch();
+  const { isError } = useSelector(state=>state.auth);
   console.log(isError);
   useEffect(() => {
+    dispatch(loadUser())
     if (isError) {
       navigate("/login");
     }
